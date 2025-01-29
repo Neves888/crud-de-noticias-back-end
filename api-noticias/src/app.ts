af7dfccd783b19262008eba0280c2cc605ae8e01
Neves888 <helen.vneves.com>
2025-01-29 05:53:37 -0300

    att

--- api-noticias/src/app.ts
+++ api-noticias/src/app.ts
@@ -3,15 +3,13 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import noticiasRouter from './routes/noticia';
 
-const app: Application = express();  // Certifique-se de que o tipo Application é utilizado
+const app: Application = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Rotas
+
 app.use('/noticias', noticiasRouter);
 
-// Conexão com o MongoDB
 mongoose.connect('mongodb://localhost:27017/noticiasdb')
     .then(() => {
         console.log('Conectado ao MongoDB');
